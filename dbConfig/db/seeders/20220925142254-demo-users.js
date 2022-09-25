84a62748bcfe4ddbb1810879072adf317de9528f
Guilherme Mazutti <mazuttimail@gmail.com>
2022-09-25 12:44:41 -0300

    populando tabela demo-users

--- dbConfig/db/seeders/20220925142254-demo-users.js
+++ dbConfig/db/seeders/20220925142254-demo-users.js
@@ -2,16 +2,62 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name:"Paulinho da DataGrupo",
+          email:"paulinhodatagrupo@test.com",
+          active:"true",
+          role:"developer",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name:"Andre de Minas",
+          email:"andre@minas.com",
+          active:true,
+          role:"developer",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name:"Alexandre Corinthiano",
+          active:true,
+          email:"alexandre@corinthians.com",
+          role:"developer",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name:"Fran de Portugal",
+          active: true,
+          email: "fran@portugal.com",
+          role:"developer",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name:"Marcelo do Rio",
+          active:true,
+          email:"marcelo@carioca.com",
+          role:"developer",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name:"Wellington Prof Temp",
+          active:true,
+          email:"prof@temp.com",
+          role:"guest",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ],
+      {}
+    );
   },
+   
 
   async down (queryInterface, Sequelize) {
     /**
