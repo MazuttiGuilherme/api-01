4ca2de8b84b7d130a8c3b797117097b4664fdee6
Guilherme Mazutti <mazuttimail@gmail.com>
2022-10-14 11:08:07 -0300

    modelo seeders events

--- dbConfig/db/seeders/20221014132231-demo-events.js
+++ dbConfig/db/seeders/20221014132231-demo-events.js
@@ -1,19 +1,45 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+  async up(queryInterface, Sequelize) {
 
-  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'People',
+      [
+        {
+          start_date: "2022-10-12",
+          name: "Rio Innovation Week",
+          user_id: 6,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          start_date: "2022-08-11",
+          name: "Fluentd for Log Data Unification",
+          user_id: 8,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          start_date: "2022-12-15",
+          name: "3rd Iberoamerican Conference on Mass Spectrometry",
+          user_id: 6,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          start_date: "2023-04-05",
+          name: "Web Summit Rio",
+          user_id: 8,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ],
+    {}
+  );
+},
+
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
