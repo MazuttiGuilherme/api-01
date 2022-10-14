c1c5af415aefdc28c44ae510dfd0af7465c12771
Guilherme Mazutti <mazuttimail@gmail.com>
2022-10-14 10:53:20 -0300

    modelo seeders users

--- dbConfig/db/seeders/20221014132137-demo-users.js
+++ dbConfig/db/seeders/20221014132137-demo-users.js
@@ -1,16 +1,86 @@
 'use strict';
 
+const { DataTypes } = require("sequelize");
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+     await queryInterface.bulkInsert(
+      'Users', 
+      [
+        {
+          name: 'Pedrinho da Datagrupo',
+          atctive: true,
+          email: 'pedrinho@datagrupo.com',
+          role: 'developer',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Guilherme da Datagrupo',
+          atctive: true,
+          email: 'guilherme@datagrupo.com',
+          role: 'developer',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Andre de Minas',
+          atctive: true,
+          email: 'andre@minas.com',
+          role: 'developer',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Alexandre Corinthiano',
+          atctive: true,
+          email: 'alexandre@corinthians.com',
+          role: 'developer',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Danilo Brum',
+          atctive: true,
+          email: 'danilo@bootcamp.com',
+          role: 'developer',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Wellington Prof. Temp',
+          atctive: true,
+          email: 'prof@temp.com',
+          role: 'guest',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Fran de Portugal',
+          atctive: true,
+          email: 'fran@portugal.com',
+          role: 'developer',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Ariel Junior',
+          atctive: true,
+          email: 'ariel@infnet.com',
+          role: 'guest',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Ana Beatriz',
+          atctive: true,
+          email: 'ana@bootcamp.com',
+          role: 'developer',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+
+      ], {});
   },
 
   async down (queryInterface, Sequelize) {
