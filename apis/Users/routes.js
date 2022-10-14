fd5257df44147cf5ad3ae80d1248e4ced777471c
Guilherme Mazutti <mazuttimail@gmail.com>
2022-10-13 21:51:08 -0300

    monitoria Wellington

--- apis/Users/routes.js
+++ apis/Users/routes.js
@@ -3,7 +3,7 @@ const route = Router();
 const usersController = require('./controllers/UsersController');
 
 
-//Users
+
 route.get("/all-trueUsers", usersController.getAllTrue);
 route.get("/all-falseUsers", usersController.getAllFalse);
 route.get('/users', usersController.getAll);
@@ -12,12 +12,6 @@ route.post("/user", usersController.createUser);
 route.put("/edit-user/:userId", usersController.editUser);
 route.delete("/delete-user/:userId", usersController.deleteUser);
 
-
-//Inscriptions
-route.get('/inscriptions', usersController.getAll);
-route.post('/inscriptions/:developer_id/user/', usersController.createUser);
-
-route.put('/inscriptions/:inscription_id/user/developer_id', usersController.editUser)
 
 
 module.exports = route;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const EventMeetingApi = require('../apis/EventMeeting/routes');
 module.exports = (app) => {
     app.get("/", (req, res) => res.send('Welcome to my API!'));
     app.use("/users-api", usersApi);
-    app.use("/inscriptions-api", EventMeetingApi);
+   // app.use("/inscriptions-api", EventMeetingApi);
 
 
 };
